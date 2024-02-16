97e83baaf68411bc029b4ff812e49357addd739e
romariofutbolist <66all@mail.ru>
2024-02-16 23:13:58 +0300

    Проверка

---
+++ src/main/java/pro/sky/SpringHwProject/SpringHwProjectApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.SpringHwProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHwProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHwProjectApplication.class, args);
+	}
+
+}
